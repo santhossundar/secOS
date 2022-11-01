8cd0b691a19541b5658d155db5d50d63556c1e3a
Santhosh <santhoshsunthar@gmail.com>
2022-11-01 23:40:39 +0530

    Some changes

--- loader.s
+++ loader.s
@@ -1,31 +1,30 @@
-global loader			;
-extern sum_of_three
+global loader
 
-MAGIC_NUMBER equ 0x1BADB002	;
-FLAGS 	     equ 0x0		;
-CHECKSUM     equ -MAGIC_NUMBER	;
+extern kmain
 
-section .text:			;
-align 4				;
-	dd MAGIC_NUMBER		;
-	dd FLAGS		;
-	dd CHECKSUM		;
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS 	     equ 0x0
+CHECKSUM     equ -MAGIC_NUMBER
 
-KERNEL_STACK_SIZE equ 4096	;
+section .text:
+align 4				
+	dd MAGIC_NUMBER
+	dd FLAGS
+	dd CHECKSUM
+
+KERNEL_STACK_SIZE equ 4096
 
 section .bss
-align 4				;
-kernel_stack:			;
-	resb KERNEL_STACK_SIZE	;
+align 4
+kernel_stack:
+	resb KERNEL_STACK_SIZE
 
-loader:				;
-	mov eax, 0xCAFEBABE	;
-	mov esp, kernel_stack + KERNEL_STACK_SIZE ;
-
-push dword 3
-push dword 2
-push dword 1
-call sum_of_three
+loader:
+	mov esp, kernel_stack + KERNEL_STACK_SIZE
+	push dword 3
+	push dword 2
+	push dword 1
+	call kmain
 
 .loop:
-	jmp .loop		;
+	jmp .loop
