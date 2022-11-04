f9f39167235aca3158727337152f756a811fb728
Santhosh <santhoshsunthar@gmail.com>
2022-11-04 15:24:40 +0530

    Writing the sentence

--- framebuffer.c
+++ framebuffer.c
@@ -1,6 +1,35 @@
-static char *fb = (char *)0x000B8000;
+#include "io.h"
+
+#define FB_COMMAND_PORT         0x3D4
+#define FB_DATA_PORT            0x3D5
+
+#define FB_HIGH_BYTE_COMMAND    14
+#define FB_LOW_BYTE_COMMAND     15
+
+#define FB_GREEN     2
+#define FB_DARK_GREY 8
+#define BLUE       1
+#define LIGHT_GREY 7
+
+void fb_move_cursor(unsigned short pos){
+        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
+        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    pos & 0x00FF);
+}
+
+static char *fb = (char *) 0x000B8000;
 
 void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
 	fb[i] = c;
 	fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+int write(char *buf, unsigned int len){
+    	unsigned int i;
+    	for(i = 0; i<len; i++){
+    		fb_write_cell(i*2, *(buf+i), LIGHT_GREY, BLUE);
+    		fb_move_cursor(2);
+    	}
+    	return i;
 }
--- kmain.c
+++ kmain.c
@@ -1,25 +1,8 @@
 #include "framebuffer.c"
-#include "io.h"
-
-#define FB_COMMAND_PORT         0x3D4
-#define FB_DATA_PORT            0x3D5
-
-#define FB_HIGH_BYTE_COMMAND    14
-#define FB_LOW_BYTE_COMMAND     15
-
-#define FB_GREEN     2
-#define FB_DARK_GREY 8
-
-void fb_move_cursor(unsigned short pos){
-        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
-        outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
-        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
-        outb(FB_DATA_PORT,    pos & 0x00FF);
-}
 
 void kmain(){
 
-	fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
-	fb_move_cursor(2);
+       	char buffer[] = "Hello! It works ...";
+       	write(buffer,sizeof(buffer));
   	
 }
